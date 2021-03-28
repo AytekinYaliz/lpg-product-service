605644d39e86bbc15d159b7b8f749c399913304c
Aytekin <aytekin@AsiyeMac.local>
2021-03-28 03:22:11 +0100

    change

--- src/test/java/com/lpg/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/lpg/productservice/ProductServiceApplicationTests.java
@@ -3,30 +3,11 @@ package com.lpg.productservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest
 class ProductServiceApplicationTests {
 
-	Calculator calculator = new Calculator();
-
-	// @Test
-	// void contextLoads() {
-	// }
-
 	@Test
-	void itShouldAdd() {
-		int result = calculator.add(12, 13);
-
-		assertEquals(25, result);
+	void contextLoads() {
 	}
 
-}
-
-
-class Calculator {
-	int add(int a, int b) {
-		return a + b;
-	}
 }
